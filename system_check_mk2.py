21efeb717a545c5b30f6c81d99f069050fa54852
owen12123 <owen12123@gmail.com>
2016-04-30 18:43:18 -0400

    can omit line
    now it can omit the line
    need to figure out how to use only useful data

--- system_check_mk2.py
+++ system_check_mk2.py
@@ -1,9 +1,21 @@
 import csv
+import itertools
+
 with open('aa.csv') as csvfile:
+	n = 0
+	for line in csvfile.readlines():
+		n += 1
+		if 'ISense 12V 50A' in line:
+			h = line.split(',')
+			break
+	csvfile.close()
+	csvfile = itertools.islice(open('aa.csv'), n-1, None)
+
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		print(row['Format'], row['MoTeC CSV File'], row['Workbook'])
+		print(row['Time'], row['ISense 12V 50A'])
 	"""
+	This is for read all the file-not very useful
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         print ', '.join(row)
