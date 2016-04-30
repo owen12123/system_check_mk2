c9d17f57b3ddece713b32e6e02f19c78908289c1
owen12123 <owen12123@gmail.com>
2016-04-30 18:06:09 -0400

    can read line
    can read lines but since the csv file has unnecessary data in first 15
    lines, need to figure out how to eliminate them or ignore when read file

--- system_check_mk2.py
+++ system_check_mk2.py
@@ -1,5 +1,10 @@
 import csv
-with open('aa.csv', 'rb') as csvfile:
+with open('aa.csv') as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		print(row['Format'], row['MoTeC CSV File'], row['Workbook'])
+	"""
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         print ', '.join(row)
+    """
