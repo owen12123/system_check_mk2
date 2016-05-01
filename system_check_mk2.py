37e1e718fdc3e0e313762ba8413d764006086222
owen12123 <owen12123@gmail.com>
2016-04-30 22:26:05 -0400

    Todo
    we need to play with dictreader and reader altogether. and for somehow,
    we cannot compare the number of row with certain boundary-in this code
    we used 0 for testing

--- system_check_mk2.py
+++ system_check_mk2.py
@@ -5,22 +5,27 @@ with open('aa.csv') as csvfile:
 	n = 0
 	for line in csvfile.readlines():
 		n += 1
-		if 'Wheel Speed FR' and 'ISense 12V 50A' in line:
-			h = line.split(',')
+		if 'ISense 12V 50A' in line:
+			#h = line.split(',')
 			break
 	csvfile.close()
 	csvfile = itertools.islice(open('aa.csv'), n-1, None)
 
+	reader_dict = csv.DictReader(csvfile)
+	number = 100
+#	for row in reader_dict:
+#		print(row['Time'], row['ISense 12V 50A'])
+#		break
+#		if row['Time'] = number
+#			print "stop"
+#			break
+	
+
 	reader = csv.reader(csvfile)
 
 	for line_list in reader:
+		if line_list[0] >= 0:
+			print 'works'
+		#print line_list[0]
 		pass
-	print line_list[0]
-	print line_list[1]
-	print line_list[15]
-
-'''
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		print(row['Time'], row['ISense 12V 50A'], row['BR2 Valid Receptions'])
-'''
+	print line_list[0] +', '+ line_list[1] +', '+ line_list[15]
