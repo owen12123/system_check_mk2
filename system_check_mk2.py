749f0d3770674d021c93613aafcbd656323ccb23
owen12123 <owen12123@gmail.com>
2016-04-30 21:26:20 -0400

    can put in list
    the last data is in list, line_list. But all the other datas are not

--- system_check_mk2.py
+++ system_check_mk2.py
@@ -11,6 +11,16 @@ with open('aa.csv') as csvfile:
 	csvfile.close()
 	csvfile = itertools.islice(open('aa.csv'), n-1, None)
 
+	reader = csv.reader(csvfile)
+
+	for line_list in reader:
+		pass
+	print line_list[0]
+	print line_list[1]
+	print line_list[15]
+
+'''
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		#print(row['Time'], row['ISense 12V 50A'], row['BR2 Valid Receptions'])
+		print(row['Time'], row['ISense 12V 50A'], row['BR2 Valid Receptions'])
+'''
