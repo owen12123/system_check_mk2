82aaeebb4520dd48bfd6ad89b887dfd70da129fd
owen12123 <owen12123@gmail.com>
2016-05-03 13:10:56 -0400

    need to import
    max and min numbers from max_min csv file

--- system_check_mk2_cleaned.py
+++ system_check_mk2_cleaned.py
@@ -11,31 +11,47 @@ def is_in_range(value, min_, max_) -> bool:
     if min_ <= value <= max_:
         return True
     return False
+# Binary select on 'expr' between 'true_' and 'false_' and return the result
+def binary_select1(expr, true_, false_):
+    if expr:
+        return true_
 
+    return false_
 
-def verify(name: str):
-    with open(name) as csvfile:
+def verify(name: str, name1: str):
+    with open(name1) as expect_csvfile:
+        for row in expect_csvfile:
+            i=0
+            irow = []
+            imax = []
+            imin = []
+            for e_ in row:
+                irow.append(binary_select1('m' in e_, e_, e_.replace(' ', '')))
+                print irow()
+                i+=1
+
+    with open(name) as csvfile_test:
         n = 0
-        for line in csvfile.readlines():
+        for line in csvfile_test.readlines():
             n += 1
             if 'ISense 12V 50A' in line:
                 # h = line.split(',')
                 break
-        csvfile.close()
-        csvfile = itertools.islice(open('aa.csv'), n - 1, None)
+        csvfile_test.close()
+        csvfile_test = itertools.islice(open('aa.csv'), n - 1, None)
 
         # this method below uses dictreader to contain the column into array
 
-        reader_dict = csv.DictReader(csvfile)
+        reader_dict = csv.DictReader(csvfile_test)
         number = 100
         for row in reader_dict:
             print(row['Time'], row['ISense 12V 50A'])
             # break
             # question: can we compare the value of each row while it is sorting
             # in this for loop?
-            if int(row['Time']) == number:
-                print("stop")
-                break
+#            if int(row['Time']) >= number:
+#               print("stop")
+#                break
 
 
-verify('aa.csv')
+verify('aa.csv', 'system_max_min.csv')
